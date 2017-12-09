d63ff9710a756f0f3f0b8d35b56c2caaf97983c0
d0man <jakub.tomasz.domanski@gmail.com>
2017-12-09 23:58:18 +0100

    added erser,toogleGrid,changeBackfround and zoom

--- designs.js
+++ designs.js
@@ -1,91 +1,108 @@
-// $(function() {
-//   var app=(function() {
-//
-//
-//     const inputHeight = $('#input_height');
-//     const inputWidth = $('#input_width');
-//     const table = $('#pixel_canvas');
-//     const $tr = $('<tr></tr>');
-//     const $td = $('<td></td>');
-//     var height = inputHeight.on("change",() =>  inputHeight.val());
-//     var width = inputHeight.on("change",() => inputWidth.val());
-//     var color = colorPicker.on("change",() => colorPicker.val());
-//
-//     return {
-//        bar: barc,
-//        heigth: height,
-//        paint: paint,
-//     }
-function makeGrid() {
-table.html('');
-colorPicker.change(()=>{color=colorPicker.val()});
-   var height=$('#input_width').val();
-   var width=$('#input_height').val();
-   var $tr = $('<tr></tr>');
-   var $td = $('<td></td>');
-   var row = '';
-   var td=''
-        for (let i = 0; i < height; i++) {
-          row += '<tr>'
-          for (let j = 0; j < width; j++) {
-              row+='<td></td>';
-          }
-          row+='</tr>'
+$(function() {
+  var app = (function() {
+    const $table = $('#pixel_canvas');
+    const $gridButton = $('#grid');
+    const $backgroundPicker = $('#bgColor');
+    const $inputHeight = $('#input_height');
+    const $inputWidth = $('#input_width');
+    const $colorPicker = $('#colorPicker');
+    const $eraserButton = $('#eraser');
+    const $zoom = $('#zoom');
+
+    var initialize = false;
+
+    return {
+      init
+    }
+
+    function makeGrid() {
+      var height = $inputHeight.val();
+      var width = $inputWidth.val();
+      var row = '';
+      $table.html('');
+      for (let i = 0; i < height; i++) {
+        row += '<tr>';
+        for (let j = 0; j < width; j++) {
+          row += '<td class="grid"></td>';
         }
-     table.append(row)
-}
+        row += '</tr>';
+      }
+      $table.append(row)
+    }
 
-function paint(){
-  $('td').on('contextmenu', function(e){
-    e.preventDefault();
-    $(this).css({background: 'transparent'});
-    return false;
-   });
- $("td").on('click',function(){
-     $(this).css({background: color});
- });
- $(document).on('mousedown' ,function() {
-   $("td").on('mouseover mouseleave',function(){
-     $(this).css({background: color});
-   });
- })
-.on('mouseup', function() {
-   $("td").off('mouseover mouseleave');
- });
-}
-//
-//      function barc(){
-//        console.log(colorPicker);
-//      }
-//   })();
-//   app.bar();
-//   console.log(app.color);
-//     console.log(app.width);
-//       $('body').click(()=>console.log(app.heigth));
-//       app.paint();
-//
-// });
-// Select color input
+    function paint() {
+      const $td = $('td');
+      var color = $colorPicker.val();
+      var colorBackup = color;
+      var isEraser = false;
+      isEraser = !isEraser;
+      $colorPicker.on('change', () => color = $colorPicker.val());
+      $eraserButton.on('click', () => {
+        $table.css({
+          cursor: 'pointer'
+        });
+        isEraser = !isEraser;
+        console.log(isEraser);
+        return isEraser ? color = colorBackup : color = 'transparent';
+      });
+      $td.on('contextmenu', function(e) {
+        e.preventDefault();
+        $(this).css({
+          background: 'transparent'
+        });
+        return false;
+      });
+      $td.on('click', function() {
+        $(this).css({
+          background: color
+        });
+      });
+      $(document).on('mousedown', function() {
+          $td.on('mouseover mouseleave', function() {
+            $(this).css({
+              background: color
+            });
+          });
+        })
+        .on('mouseup', function() {
+          $td.off('mouseover mouseleave');
+        });
+    }
 
+    function changeBackground() {
+      var bgColor = $backgroundPicker.val('#FCFFCF');
+      $backgroundPicker.on('change', () => {
+        bgColor = $backgroundPicker.val()
+        $table.css({
+          background: bgColor
+        })
+      });
+    }
 
-// colorPicker.change(()=>{color=colorPicker.val()});
-// inputHeight.change(()=>{canvasHeight = inputHeight.val();});
-// inputWidth.change(()=>{canvasWidth = inputWidth.val();})
+    function toggleGrid() {
+      var $td = $('td');
+      $gridButton.on('click', () => $td.toggleClass('grid'));
+    }
 
-// When size is submitted by the user, call makeGrid()
-const colorPicker = $('#colorPicker');
- const table = $('#pixel_canvas');
-var color = '#000';
+    function zoom() {
+      $zoom.on('click', () => $table.toggleClass('zoom'));
+    }
 
+    function init() {
+      if (initialize) {
+        return;
+      }
+      initialize = true;
+      changeBackground();
+      $('#submit').click(function(e) {
+        e.preventDefault();
+        makeGrid();
+        paint();
+        toggleGrid();
+        zoom();
+      });
+    }
+  })();
 
-       // $('td').mousedown(function() {
-       // });
-       //   $(this).css({background:"#333333"});
-
-
-
-$('#submit').click(function (e) {
-    e.preventDefault();
-    makeGrid();
-    paint();
+  app.init()
 });
