879fca89c6264ab1e30a2b0cf1ba64793b50938f
Carlo Santos <carlox7@gmail.com>
2018-01-29 01:10:27 -0800

    Feature: Grid, Color Picker, Size Picker. Finished Grid function that takes input value and creates grid. Color Picker is MVP status. Still adding refinements to Color Picker.

--- designs.js
+++ designs.js
@@ -2,9 +2,34 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+let sizeForm = $('#sizePicker');
 
-function makeGrid() {
+function pickColor(e){
+  e.preventDefault();
+  let color = $('#colorPicker');
+  let colorPick = color.val();
+  console.log(colorPick);
+  $(this).css('background-color', colorPick);
+}
 
-// Your code goes here!
+function makeGrid(e) {
+  e.preventDefault();
+  let canvas = $('#pixelCanvas');
+  let height = $('#inputHeight').val();
+  let width = $('#inputWidth').val();
+  let tableRows = '';
+
+  for(var i = 0; i < height; i++){
+    tableRows += '<tr>';
+    for(var j = 0; j < width; j++){
+      tableRows += '<td class=""></td>';
+    }
+    tableRows += '</tr>';
+  }
+  canvas.html(tableRows);
+
+  canvas.delegate('td', 'mousedown', pickColor);
 
 }
+
+sizeForm.submit(makeGrid);
