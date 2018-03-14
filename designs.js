68e96a29d4a99803c37d9b58a4784bcf40200b3a
GarrettW <garrettwelson@gmail.com>
2018-03-13 23:15:04 -0400

    Working Prototype
    My site can now do the following:
    
    1) Draw a grid and re-draw based on the submit button
    
    2) Select a color
    
    3) Fill in table cells on clicks

--- designs.js
+++ designs.js
@@ -2,10 +2,13 @@
 
 function makeGrid(height, width) {
 
+	idCount = 1;
+
 	for (var i =0; i < height; i++) {
-		var row = $("<tr></tr>").appendTo("table");
+		var row = $("<tr></tr>").appendTo("#pixelCanvas");
 		for (var j = 0; j < width; j++) {
-			$('<td></td>').text("test").appendTo("#pixelCanvas");
+			$('<td></td>').text(" ").addClass("cell").attr("id",idCount).appendTo(row);
+			idCount++;
 		}
 	}
 
@@ -13,16 +16,29 @@ function makeGrid(height, width) {
 
 // Select color input
 
+color = "#ffffff"
 
 $("#colorPicker").change(function() {
-	var color = $("#colorPicker").val();
-	$("h2").css('color', color);
+	color = $("#colorPicker").val();
+	
 })
 
 // Select size input
 
-$("#sizePicker").submit(function(){
+$("#sizePicker").submit(function(event){
+	event.preventDefault();
+
+	if ($("#pixelCanvas").children().length >= 1) {
+		$("#pixelCanvas").empty();
+	}
+
 	height = $("#inputHeight").val();
 	width = $("#inputWidth").val();
 	makeGrid(height, width)
+	
+})
+
+$("#pixelCanvas").click(function(event) {
+        var cellToColor = event.target.id;
+        $('#' + cellToColor).css("background-color", color);
 })
