2380e143a82a6379da67cf9b821aae191ac66d59
suman <suman@fusemachines.com>
2018-03-25 17:26:13 -0400

    table grid created;
    for loop implement to get the value submtted and create the pixel grid

--- designs.js
+++ designs.js
@@ -3,8 +3,40 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+function makeGrid(rw,col) {
+
+$("#pixelCanvas").empty();
+	//	$("#pixelCanvas").prepend(rw);
+	for(var i=1; i<=rw; i++){
+		
+		var row="<tr id='r"+i+"'>";
+		$("#pixelCanvas").append(row);
+		for(var y=1; y<=col; y++){
+			var cell="<td id='r"+i+"c"+y+"'>";
+			$("tr#r"+i).append(cell);			
+		}
+	}
+
+		
+
+	
 
 // Your code goes here!
 
 }
+
+jQuery(document).ready(function() {
+
+
+	$("#sizePicker").on("submit",function(e){
+var rw = $("#inputHeight").val();
+var col = $("#inputWidth").val();
+		makeGrid(rw,col);
+		e.preventDefault();
+}); //end of sizePicker
+
+	
+}); //end of ready();
+
+
+	
