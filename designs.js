35e63b4c4b3541cdb5d0d8aad6e6b577a4247588
suman <suman@fusemachines.com>
2018-03-25 17:47:17 -0400

    color the cell done

--- designs.js
+++ designs.js
@@ -3,39 +3,58 @@
 
 // When size is submitted by the user, call makeGrid()
 
+
+
+jQuery(document).ready(function() {
+var color="#000";
+
+
 function makeGrid(rw,col) {
 
 $("#pixelCanvas").empty();
 	//	$("#pixelCanvas").prepend(rw);
 	for(var i=1; i<=rw; i++){
 		
-		var row="<tr id='r"+i+"'>";
+		var row="<tr id='r"+i+"' class='row'>";
 		$("#pixelCanvas").append(row);
 		for(var y=1; y<=col; y++){
-			var cell="<td id='r"+i+"c"+y+"'>";
+			var cell="<td id='r"+i+"c"+y+"' class='cell'>";
 			$("tr#r"+i).append(cell);			
 		}
 	}
 
-		
+}
 
-	
 
-// Your code goes here!
+function colorCell(id,color){
+	$("#"+id).css("background",color);
 
 }
 
-jQuery(document).ready(function() {
-
-
-	$("#sizePicker").on("submit",function(e){
+$("#sizePicker").on("submit",function(e){
 var rw = $("#inputHeight").val();
 var col = $("#inputWidth").val();
-		makeGrid(rw,col);
-		e.preventDefault();
+color=$("#colorPicker").val();
+
+makeGrid(rw,col);
+console.log("row: "+rw+" col: "+col+" Color: "+color);
+e.preventDefault();
 }); //end of sizePicker
 
-	
+
+// $("#colorPicker").click(function(){
+// 	color=$("#colorPicker").val();
+// });
+
+
+$("#pixelCanvas").on("click",".cell",function(){
+var id=$(this).attr("id");
+color = $("#colorPicker").val(); 
+colorCell(id,color);
+});
+
+
+
 }); //end of ready();
 
 
