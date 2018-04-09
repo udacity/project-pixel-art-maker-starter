3b63b7d0042529bd7481cda8e62677c8a9cda503
Anastasia Isaeva <anastasiya.isaeva@gmail.com>
2018-04-08 19:41:28 -0500

    Yay pixel art completed

--- designs.js
+++ designs.js
@@ -3,8 +3,39 @@
 
 // When size is submitted by the user, call makeGrid()
 
+var gridHeight = $('#inputHeight').val();
+var gridWidth = $('#inputWeight').val();
+
+$('#sizePicker').submit(function(evt) {
+    evt.preventDefault();
+
+    gridHeight = $('#inputHeight').val();
+    gridWidth = $('#inputWeight').val();
+    
+    // Create grid
+    makeGrid();
+
+    // Assign event listeners to color cells
+    $('td').click(function(e) {
+        if ($(e.target).css('background-color') === 'rgba(0, 0, 0, 0)') {
+            $(e.target).css('background-color', $('#colorPicker').val());
+        } else {
+            $(e.target).css('background-color', '');
+        }
+    });
+});
+
 function makeGrid() {
+    $('#pixelCanvas').empty();
 
-// Your code goes here!
-
+    var row = '';
+    for (var i = 0; i < gridWidth; i++) {
+        row += '<td></td>';
+    }
+    var table = '';
+    for (var i = 0; i < gridHeight; i++) {
+        table += '<tr>' + row + '</tr>';
+    }
+    
+    $('#pixelCanvas').append(table);
 }
