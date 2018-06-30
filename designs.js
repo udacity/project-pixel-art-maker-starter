2b4943eea5e826a227c77f1cd7611ff06a461c95
Abdulsalam Kabir <40652857+Mr-Prime01@users.noreply.github.com>
2018-06-30 16:00:47 -0700

    Design.js

--- designs.js
+++ designs.js
@@ -6,5 +6,26 @@
 function makeGrid() {
 
 // Your code goes here!
+    var val = document.getElementById("sizePicker");
+
+        var h = val.elements[0].value;
+        var w = val.elements[1].value;
+
+            document.getElementById("pixelCanvas").innerHTML = h;
+            document.getElementById("test").innerHTML = w;
+        
+  var colz = document.getElementById("sizePicker");
+  
+        var color = val.elements[0].value;
+
+       while(h > 0){
+
+            var row = "<tr>";
+
+            while(w > 0){
+	            var col = "<td> </td>";
+            }
+        }
+
 
 }
