766affa21f2eb3a433199efbfc0676b7e63e124b
katrink66 <39248380+katrink66@users.noreply.github.com>
2018-07-02 01:36:43 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -6,26 +6,25 @@
 
 
 // Your code goes here!
-$(document).ready(function(){
-  $('#sizePicker').submit(function makeGrid(grid){
+$(document).ready(function() {
+  $('#sizePicker').submit(function makeGrid(grid) {
     $('table tr').remove();
-    var rows=$('#input_height').val();        
-    var columns=$('#input_width').val();
-      for(var i=1;i<=rows;i++){
+    var rows = $('#input_height').val();        
+    var cols = $('#input_width').val();
+      for(var i = 1;i <=rows; i++) {
         $('table').append('<tr></tr>');
-       for(var j=1;j<=columns; j++){
+        for(var j = 1;j <=cols; j++) {
          $('tr:last').append('<td></td>');
-         $('td').attr("class",'cells');
+         $('td').attr("class", 'cells');
+        }
+      }
             grid.preventDefault();
-            $('.cells').click(function(event){
-              var paint=$('#colorPicker'),val();
-              $(event.target).css('background-color',paint);
-    
-            }
-            }
-});
-       }
-       }  
+            $('.cells').click(function(event) {
+              var paint=$('#colorPicker').val();
+              $(event.target).css('background-color', paint);
+            });
+  });
+}); 
   
   
 /*make grid*/
