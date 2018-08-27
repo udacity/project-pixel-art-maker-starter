85c4868240fa1578554976574d55a7956b623a6d
Charul Bari <baricharul9@gmail.com>
2018-08-28 03:18:48 +0530

    Logic to make grid!

--- designs.js
+++ designs.js
@@ -5,9 +5,17 @@ let newTable=document.getElementById('pixelCanvas');
 let size=document.getElementById('sizePicker');
 let color= document.getElementById('colorPicker');
 
-
+size.addEventListener('submit',function(event){
+ event.preventDefault();
+ makeGrid();
+});
 function makeGrid() {
-
-// Your code goes here!
-
+  for(let i=0;i<ht.value;i++)
+    {
+      const row=newTable.insertRow(i);
+      for(let j=0;j<wd.value;j++)
+        {
+          const cell=row.insertCell(j);
+        }
+    }
 }
