18f4b2f820f016e5fbe21a80a9508d07e76364b3
hv539a <47986334+hv539a@users.noreply.github.com>
2019-03-19 02:28:13 -0700

    Update design node.js
    Please find the updated code for the same.

--- designs.js
+++ designs.js
@@ -1,10 +1,47 @@
 // Select color input
 // Select size input
+var height, width, color;
+
 
 // When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function (event) {
+    event.preventDefault();
+    height = $('#inputHeight').val();
+    width = $('#inputWidth').val();
+    makeGrid(height, width);
+    //console.log('Height: '+ height + ' and width: ' + width);
+})
 
-function makeGrid() {
+    
+  
+
+function makeGrid(x, y) {
+  $('tr').remove();
+  
 
 // Your code goes here!
+ for (var i = 1; i <= x; i++) {
+     $(' #pixelCanvas').append('<tr id=table' + i + '></tr>');
+     for (var j = 1; j <=y; j++) {
+         $('#table' + i).append('<td></td>');
+     }
+ }
+  
+  
+ // add color to cell
+ $('td').click(function addcolor() {
+     color = $('#colorPicker').val();
+   
+    if ($(this).attr('style')) {
+        $(this).removeAttr('style')
+    } else {
+        $(this).attr('style', 'background-color:' + color);
+    }
+ })
+}
+   
+  
+   
+  
 
 }
