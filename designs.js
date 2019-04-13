1419bd701a8ae0cecb338071b1df0daba4370834
Shubham Prakash Jha <prakash.shubhamjha@gmail.com>
2019-04-13 12:15:11 +0530

    Print value stored in the variables

--- designs.js
+++ designs.js
@@ -7,6 +7,14 @@ function makeGrid(event) {
 
 // Your code goes here!
     event.preventDefault();
+
+    const m = document.querySelector('#inputHeight').value;
+    const n = document.querySelector('#inputWidth').value;
+    const color = document.querySelector('input#colorPicker').value;
+    const pixelTable = document.querySelector('#pixelCanvas');
+
+    
+    console.log(`The size is ${m}*${n} and color is ${color}`);
 }
 
 const form = document.querySelector('form#sizePicker');
