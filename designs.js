fb9f3f813a9f1c6cf853879b084f2e80874c30f9
Autumn <autumn.m.peters@gmail.com>
2019-04-28 09:46:19 -0700

    colorFix

--- designs.js
+++ designs.js
@@ -1,12 +1,28 @@
 // Select color input
-var colorPicker = document.getElementById("colorPicker");
-canvas.addEventListener("submit", makeGrid);
+var colorPicker = document.getElementById("colorPicker").value;
 
 // Select size input
 var sizePicker = document.getElementById("sizePicker");
+sizePicker.addEventListener("submit", function(event) {
+  event.preventDefault();
+  makeGrid();
+});
 
 // When size is submitted by the user, call makeGrid()
 function makeGrid() {
   var inputHeight = document.getElementById("inputHeight").value;
   var inputWidth = document.getElementById("inputWidth").value;
+  var canvas = document.getElementById("canvas");
+  canvas.innerHTML = "";
+  for (let x = 0; x < inputHeight; x++) {
+    var row = document.createElement("tr");
+    canvas.appendChild(row);
+    for (let y = 0; y < inputWidth; y++) {
+      var column = document.createElement("td");
+      row.appendChild(column);
+    }
+  }
+
+  console.log("called function make grid");
+  console.log(colorPicker);
 }
