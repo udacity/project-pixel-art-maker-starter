69b1bc530ad8b9b3e871348bf5708b39c3583e2c
mbc9863 <46851311+mbc9863@users.noreply.github.com>
2020-05-17 11:48:47 -0700

    feat: Add JavaScript Code to Project
    Add JavaScript code to enable functionality for HTML and CSS for Udacity Pixel Art Maker Project

--- designs.js
+++ designs.js
@@ -1,10 +1,37 @@
-// Select color input
+const $tableElement = $('#pixelCanvas');
+const $inputHeight = $('#inputHeight');
+const $inputWidth = $('#inputWidth');
+const $colorPicker = $('#colorPicker');
+
 // Select size input
+$('#sizePicker').submit( event => {
+    event.preventDefault();
+
+    let width = $inputWidth.val();
+    let height = $inputHeight.val();
+
+// Clear
+$tableElement.html(''); 
 
 // When size is submitted by the user, call makeGrid()
+    makeGrid(height, width);
+    addCellClickListener();
+});
 
-function makeGrid() {
+function makeGrid(height, width) {
+    for(let i = 0; i < height; i++) {
+        $tableElement.append('<tr></tr>');
+    };
 
-// Your code goes here!
+    for(let i = 0; i < width; i++) {
+        $('tr').append('<td></td>');
+    };
+};
 
-}
+// Select color input
+function addCellClickListener() {
+    $('td').click( event => {
+        let color = $colorPicker.val();
+        $(event.currentTarget).css("background-color", color)
+    });
+};
