ec17dd06346a987be3738b1b011eb148578aa1c1
jesusreyes01 <jesusareyes01@gmail.com>
2021-01-21 13:06:41 -0600

    feature-added mousedown drawing functionality

--- designs.js
+++ designs.js
@@ -3,24 +3,101 @@ let color = document.getElementById('colorPicker');
 let height = document.getElementById('inputHeight');
 let width = document.getElementById('inputWidth');
 let sizePickerForm = document.getElementById('sizePicker');
+let eraseMode = document.querySelector('.erase-mode');
+let drawMode = document.querySelector('.draw-mode');
+let fillButton = document.querySelector('.fill-button');
 
 let makeGrid = (event) => {
     event.preventDefault()
+    //Deletes old table
     if(gridTable.rows.length > 0){
         while(gridTable.rows.length > 0){
             gridTable.deleteRow(0);
         }
     }
-
-    for (let i=0; i<height.value; i++){
+    //Creates table
+    for (let i=0; i < height.value; i++){
         const row = gridTable.insertRow(i);
-        for (let j=0; j<width.value; j++){
+        for (let j=0; j < width.value; j++){
             const cell = row.insertCell(j);
-            cell.addEventListener('click', () => {
-                cell.style.backgroundColor = color.value;
+            cell.addEventListener('mousedown', e =>  {
+                e.target.style.backgroundColor = color.value;
             });
         }
     }
 }
-
+//Listens for submit input on form.
 sizePickerForm.addEventListener('submit', makeGrid);
+
+let down = false;
+
+gridTable.addEventListener('mousedown', (e) => {
+	down = true;
+	gridTable.addEventListener('mouseup', () => {
+		down = false;
+	});
+    gridTable.addEventListener('mouseleave', () => {
+        down = false;
+     });
+
+    gridTable.addEventListener('mouseover', e => {
+  	    if (down) {
+            if (e.target.tagName === 'TD') {
+                e.target.style.backgroundColor = color.value;
+            }
+        }
+    });
+});
+
+pixelCanvas.addEventListener('dblclick', e => {
+    e.target.style.backgroundColor = null;
+});
+
+eraseMode.addEventListener('click', () => {
+    gridTable.addEventListener('mousedown', (e) => {
+        down = true;
+        gridTable.addEventListener('mouseup', () => {
+            down = false;
+        });
+        gridTable.addEventListener('mouseleave', () => {
+            down = false;
+        });
+
+        gridTable.addEventListener('mouseover', e => {
+            if (down) {
+                if (e.target.tagName === 'TD') {
+                    e.target.style.backgroundColor = null;
+                }
+            }
+        });
+    });
+    pixelCanvas.addEventListener('mousedown', (e) => {
+        e.target.style.backgroundColor = null;
+    });
+  });
+
+drawMode.addEventListener('click', () => {
+    gridTable.addEventListener('mousedown', (e) => {
+            down = true;
+            gridTable.addEventListener('mouseup', () => {
+                down = false;
+            });
+            gridTable.addEventListener('mouseleave', () => {
+                down = false;
+            });
+            gridTable.addEventListener('mouseover', e => {
+                if (down) {
+                    if (e.target.tagName === 'TD') {
+                        e.target.style.backgroundColor = color.value;
+                    }
+                }
+            });
+    });
+    gridTable.addEventListener('mousedown', (e) => {
+        e.target.style.backgroundColor = color.value;
+    });
+  });
+
+  fillButton.addEventListener('click', () => {
+    gridTable.querySelectorAll('td').forEach( td => td.style.backgroundColor = color.value);
+  });
