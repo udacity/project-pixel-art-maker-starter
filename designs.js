794c8a440b0b3a70438e4be7676b911489db88fc
jesusreyes01 <jesusareyes01@gmail.com>
2021-01-20 21:43:36 -0600

    finished designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,29 @@
-// Select color input
-// Select size input
+let gridTable = document.getElementById('pixelCanvas');
+let color = document.getElementById('colorPicker');
+let height = document.getElementById('inputHeight');
+let width = document.getElementById('inputWidth');
+let sizePickerForm = document.getElementById('sizePicker');
 
-// When size is submitted by the user, call makeGrid()
+sizePickerForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    resetGrid()
+    makeGrid()
+}, false);
 
-function makeGrid() {
+let makeGrid = () => {
+    for (let i=0; i<height.value; i++){
+        const row = gridTable.insertRow(i);
+        for (let j=0; j<width.value; j++){
+            const cell = row.insertCell(j);
+            cell.addEventListener('click', function() {
+                this.style.backgroundColor = color.value; 
+            });
+        }
+    }
+}
 
-// Your code goes here!
-
+let resetGrid = () => {
+    while (gridTable.rows.length > 0) {
+        gridTable.deleteRow(0);
+    }
 }
