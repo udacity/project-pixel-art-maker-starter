7cd0093a3b5d06f77aa0b1b94d235284edead281
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-21 21:16:18 -0600

    turn const to var

--- designs.js
+++ designs.js
@@ -3,20 +3,21 @@
 
 // When size is submitted by the user, call makeGrid()
 
-const colorPicker = document.getElementById("colorPicker");
+
 
 function formSubmit(event) {
     event.preventDefault(); // stops form submission action
-    const table = document.getElementById('pixelCanvas');
+    var table = document.getElementById('pixelCanvas');
     table.innerHTML = "";  // table element gets cleared here
-	const rows = document.getElementById("inputHeight").value;
-    const cols = document.getElementById("inputWidth").value;
+	var rows = document.getElementById("inputHeight").value;
+    var cols = document.getElementById("inputWidth").value;
     makeGrid(rows, cols);
     return false;
 }
 
 function makeGrid(rows, cols) {
-	const table = document.getElementById('pixelCanvas');
+    var table = document.getElementById('pixelCanvas');
+    var colorPicker = document.getElementById("colorPicker");
     for (r=0; r<rows; r++) {
         var row = table.insertRow(r);
         for (c=0; c<cols; c++) {
