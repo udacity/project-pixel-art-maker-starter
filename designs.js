653b8a5d8c11870ec9490c19b6e91627d9fd082d
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-21 19:26:16 -0600

    Update designs.js, got size picker working

--- designs.js
+++ designs.js
@@ -4,31 +4,29 @@
 // When size is submitted by the user, call makeGrid()
 
 const submit = document.getElementById('submitButton');
-const table = document.getElementById('pixelCanvas');
 const color = document.getElementById("colorPicker");
 
 
-function formSubmit(e) {
-    e.preventDefault();
-    const rows = document.getElementById("inputHeight").value;
+function formSubmit(event) {
+	event.preventDefault();
+	const rows = document.getElementById("inputHeight").value;
     const cols = document.getElementById("inputWidth").value;
-    makeGrid(rows,cols);
+    makeGrid(rows, cols);
+    return false;
 }
 
 function makeGrid(rows, cols) {
-    
+	const table = document.getElementById('pixelCanvas');
     for (r=0; r<rows; r++) {
         var row = table.insertRow(r);
         for (c=0; c<cols; c++) {
-            var cell = row.insertCell(c);
-            cell.textContent = r+":"+c;
+        	var cell = row.insertCell(c);
+            // cell.textContent = r+":"+c;
             cell.addEventListener("click", respondToClick)
-        };
+        }
     };
 };
 
 function respondToClick(e) {
     console.log("A cell was clicked: "+e.target.textContent)
 }
-
-makeGrid(10,10);
