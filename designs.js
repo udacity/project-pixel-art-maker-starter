5bdaa70580103e8311824d6ec24d45467d32d5b9
MsRoxie <98505343+MsRoxie@users.noreply.github.com>
2022-02-28 00:05:11 -0500

    Fix typo

--- designs.js
+++ designs.js
@@ -1,10 +1,40 @@
-// Select color input
-// Select size input
+let height = document.getElementById("input_height");
+let width = document.getElementById("input_width");
+let sizePicker = document.getElementById("sizePicker");
+let color = document.getElementById("colorPicker");
+let canvas = document.getElementById("pixel_canvas");
 
-// When size is submitted by the user, call makeGrid()
+color.addEventListener("click", function(){});
+
+sizePicker.onsubmit = function(event){
+    event.preventDefault();
+
+    makeGrid();
+};
 
 function makeGrid() {
+    for (let r=0; r<height.value; r++){
+        const row = canvas.insertRow(r);
+        for (let c=0; c<width.value; c++){
+            const cell = row.insertCell(c);
+            cell.addEventListener("click", fillSquare);
+        }
+    }
+}
 
-// Your code goes here!
+{
+    while (canvas.firstChild){
+         canvas.removeChild(canvas.firstChild);
+    }
+}
 
+function clearGrid(){
+    while (canvas.firstChild){
+         canvas.removeChild(canvas.firstChild);
+    }
+}
+
+
+function fillSquare () {
+    this.setAttribute("style", `background-color: ${color.value}`);
 }
