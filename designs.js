9afa6313a6d30bd57bc2edb4d3244c6ca3a41153
Diamond Linde <diamondlinde3@gmail.com>
2022-05-18 14:16:37 -0400

    Add comments

--- designs.js
+++ designs.js
@@ -2,29 +2,29 @@ const size = document.getElementById('sizePicker');
 const canvas = document.getElementById("pixelCanvas");
 
 function makeGrid() {
-  const height = document.getElementById("inputHeight").value;
-  const width = document.getElementById("inputWidth").value;
+	const height = document.getElementById("inputHeight").value; // get height input
+	const width = document.getElementById("inputWidth").value; // get width input
 
-  for (let x = 0; x < height; x++) {
-    const tr = canvas.insertRow(x);
-    for (let y = 0; y < width; y++) {
-      const td = tr.insertCell(y);
-      
-      td.addEventListener("click", evt => {
-        var color = document.querySelector("#colorPicker").value; 
-        evt.target.style.backgroundColor = color; // Adds color when cell is clicked
-      });
-    
-      td.addEventListener('dblclick', evt => {
-        evt.target.style.backgroundColor = null; // Removes color when cell is double-clicked
-      });
-    }
-  }
+	for (let x = 0; x < height; x++) {
+		const tr = canvas.insertRow(x); // Adds rows
+		for (let y = 0; y < width; y++) {
+			const td = tr.insertCell(y); // Adds each cell 
+
+			td.addEventListener("click", evt => {
+				var color = document.querySelector("#colorPicker").value;
+				evt.target.style.backgroundColor = color; // Adds color when cell is clicked
+			});
+
+			td.addEventListener('dblclick', evt => {
+				evt.target.style.backgroundColor = null; // Removes color when cell is double-clicked
+			});
+		}
+	}
 }
 size.onclick = (evt) => {
-  evt.preventDefault();
-  while (canvas.hasChildNodes()) {
-    canvas.removeChild(canvas.lastChild);
-}
-  makeGrid();
+	evt.preventDefault(); // Prevents the page from refreshing when sizes are submitted
+	while (canvas.hasChildNodes()) {
+		canvas.removeChild(canvas.lastChild);// clears any previous grid away
+	}
+	makeGrid(); // Calls makeGrid() when the sizes are submitted 
 };
