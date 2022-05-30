7740e79ef533922062d4d47d4cfb750ff79956c7
lnginyard <lnginyard@gmail.com>
2022-05-30 13:26:59 -0400

    Update designs.js
    begin to enter code in design.js

--- designs.js
+++ designs.js
@@ -1,10 +1,11 @@
 // Select color input
-// Select size input
+"colorPicker" = document.querySelector(color)
+    // Select size input
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
 
-// Your code goes here!
+    // Your code goes here!
 
 }
