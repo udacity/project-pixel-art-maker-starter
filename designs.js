fdea6eca39b7543893249b512d4c4d9788943430
adityalochan <aditya.lochan25@gmail.com>
2022-06-28 00:41:22 -0400

    adding event listener to submit

--- designs.js
+++ designs.js
@@ -2,9 +2,17 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+const height = document.querySelector('#inputHeight');
+const width = document.querySelector('#inputWidth');
+
+const submit = document.querySelectorAll('input[type=submit]')
+
+submit.addEventListener('click', makeGrid());
+
 
 function makeGrid() {
-
 // Your code goes here!
+
+  console.log('The heading was clicked!');
 
 }
