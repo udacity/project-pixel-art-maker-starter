11ca79b593d378949deb68374bd1e0753261f7a2
chipsara <113566896+chipsara@users.noreply.github.com>
2022-09-20 15:09:54 -0400

    Update designs.js
    js update

--- designs.js
+++ designs.js
@@ -1,10 +1,40 @@
-// Select color input
+const sizePicker = document.getElementById("sizePicker");
 // Select size input
+let inputHeight = document.getElementById("inputHeight");
+let inputWidth = document.getElementById("inputWidth");
 
-// When size is submitted by the user, call makeGrid()
+// Select color input
+let color = document.getElementById("colorPicker");
 
-function makeGrid() {
+const pixelCanvas = document.getElementById("pixelCanvas");
 
-// Your code goes here!
-
+function clearGrid() {
+  const allRows = document.querySelectorAll("tr");
+  allRows.forEach((row) => {
+    row.remove();
+  });
 }
+
+function makeGrid(e) {
+  e.preventDefault();
+
+  clearGrid();
+
+  const height = inputHeight.value;
+  const width = inputWidth.value;
+  for (let i = 1; i <= height; i++) {
+    const row = document.createElement("tr");
+    for (let j = 1; j <= width; j++) {
+      const column = document.createElement("td");
+      column.id = "column-i-j";
+      row.appendChild(column);
+    }
+    pixelCanvas.appendChild(row);
+  }
+}
+
+sizePicker.addEventListener("submit", makeGrid);
+
+pixelCanvas.addEventListener("click", function (e) {
+  e.target.style.backgroundColor = color.value;
+});
